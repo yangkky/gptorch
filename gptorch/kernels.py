779a734341d4aac583005bdf79569251e8b61c70
yangkky <seinchin@gmail.com>
2018-08-31 18:40:02 -0700

    Cache computations in FixedWDK.

--- gptorch/kernels.py
+++ gptorch/kernels.py
@@ -212,18 +212,24 @@ class FixedWDK(WeightedDecompositionKernel):
         self.S = S
         self.graph = self.make_graph(contacts, L)
         self.graph.to(self.S.device)
+        self.saved = {}
 
     def forward(self, X1, X2):
-        n1, L = X1.size()
-        n2, _ = X2.size()
-        subs = self.S[X1, X1]
-        k1 = self.wdk(subs).view((n1, 1))
-        subs = self.S[X2, X2]
-        k2 = self.wdk(subs).unsqueeze(0)
-        L_inds = torch.arange(L).long()
-        subs = self.S[X1][:, L_inds, X2].view((n1 * n2, L))
-        K = self.wdk(subs).view((n1, n2))
-        return (self.a ** 2) * (K / torch.sqrt(k1) / torch.sqrt(k2)) ** self.gamma
+        if (X1, X2) in self.saved:
+            K = self.saved[(X1, X2)]
+        else:
+            n1, L = X1.size()
+            n2, _ = X2.size()
+            subs = self.S[X1, X1]
+            k1 = self.wdk(subs).view((n1, 1))
+            subs = self.S[X2, X2]
+            k2 = self.wdk(subs).unsqueeze(0)
+            L_inds = torch.arange(L).long()
+            subs = self.S[X1][:, L_inds, X2].view((n1 * n2, L))
+            K = self.wdk(subs).view((n1, n2))
+            K = (K / torch.sqrt(k1) / torch.sqrt(k2))
+            self.saved[(X1, X2)] = K
+        return (self.a ** 2) * K ** self.gamma
 
 
 class SoftWeightedDecompositionKernel(BaseKernel):
