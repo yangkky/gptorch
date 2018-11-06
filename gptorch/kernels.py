12865924d048d55f27455bb4b97727d7b0849d2b
yangkky <seinchin@gmail.com>
2018-11-06 09:39:02 -0800

    Use repr to hash tensors.

--- gptorch/kernels.py
+++ gptorch/kernels.py
@@ -213,18 +213,23 @@ class FixedWDK(WeightedDecompositionKernel):
         self.S = S
         self.graph = self.make_graph(contacts, L)
         self.graph.to(self.S.device)
+        self.dic = {}
 
     def forward(self, X1, X2):
-        n1, L = X1.size()
-        n2, _ = X2.size()
-        subs = self.S[X1, X1]
-        k1 = self.wdk(subs).view((n1, 1))
-        subs = self.S[X2, X2]
-        k2 = self.wdk(subs).unsqueeze(0)
-        L_inds = torch.arange(L).long()
-        subs = self.S[X1][:, L_inds, X2].view((n1 * n2, L))
-        K = self.wdk(subs).view((n1, n2))
-        K = (K / torch.sqrt(k1) / torch.sqrt(k2))
+        if (repr(X1), repr(X2)) in self.dic:
+            K = self.dic[repr(X1), repr(X2)]
+        else:
+            n1, L = X1.size()
+            n2, _ = X2.size()
+            subs = self.S[X1, X1]
+            k1 = self.wdk(subs).view((n1, 1))
+            subs = self.S[X2, X2]
+            k2 = self.wdk(subs).unsqueeze(0)
+            L_inds = torch.arange(L).long()
+            subs = self.S[X1][:, L_inds, X2].view((n1 * n2, L))
+            K = self.wdk(subs).view((n1, n2))
+            K = (K / torch.sqrt(k1) / torch.sqrt(k2))
+            self.dic[repr(X1), repr(X2)] = K
         return (self.a ** 2) * (K ** self.gamma)
 
 
